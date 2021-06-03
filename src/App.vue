7237ab6031b7a558b7ea150329ecf3f025d2ca53
LittleDC <1187061276@qq.com>
2021-06-03 11:31:34 +0800

    table base

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,24 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/test3">test3</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
+/* 引入element-ui css文件 坑爹 */
+@import url("https://unpkg.com/element-ui/lib/theme-chalk/index.css");
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  padding: 0 30px;
 }
 
 #nav {
